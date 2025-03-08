746ff4f41ca2a9492667dc80533c36d9df7ff4bc
Mafaz03 <mohdmafaz200303@gmail.com>
2025-03-08 14:42:12 +0530

    wrote automation code

--- scripts.py
+++ scripts.py
@@ -132,6 +132,85 @@ function evaluatePixel(sample) {
 }
 """
 
+evalscript_NDWI = """
+//VERSION=3
+//ndwi
+const colorRamp1 = [
+  	[0, 0x000000],
+  	[1, 0x008000]
+  ];
+const colorRamp2 = [
+  	[0, 0x000000],
+  	[1, 0x0000CC]
+  ];
+
+let viz1 = new ColorRampVisualizer(colorRamp1);
+let viz2 = new ColorRampVisualizer(colorRamp2);
+
+function setup() {
+  return {
+    input: ["B03", "B08", "SCL","dataMask"],
+    output: [
+		{ id:"default", bands: 4 },
+        { id: "index", bands: 1, sampleType: "FLOAT32" },
+        { id: "eobrowserStats", bands: 2, sampleType: 'FLOAT32' },
+        { id: "dataMask", bands: 1 }
+	]
+  };
+}
+
+function evaluatePixel(samples) {
+  let val = index(samples.B03, samples.B08);
+  let imgVals = null;
+  // The library for tiffs works well only if there is only one channel returned.
+  // So we encode the "no data" as NaN here and ignore NaNs on frontend.
+  const indexVal = samples.dataMask === 1 ? val : NaN;
+  
+  if (val < -0) {
+    imgVals = [...viz1.process(-val), samples.dataMask];
+  } else {
+    imgVals = [...viz2.process(Math.sqrt(Math.sqrt(val))), samples.dataMask];
+  }
+  return {
+    default: imgVals,
+    index: [indexVal],
+    eobrowserStats:[val,isCloud(samples.SCL)?1:0],
+    dataMask: [samples.dataMask]
+  };
+}
+
+
+function isCloud(scl) {
+  if (scl == 3) {
+    // SC_CLOUD_SHADOW
+    return false;
+  } else if (scl == 9) {
+    // SC_CLOUD_HIGH_PROBA
+    return true;
+  } else if (scl == 8) {
+    // SC_CLOUD_MEDIUM_PROBA
+    return true;
+  } else if (scl == 7) {
+    // SC_CLOUD_LOW_PROBA
+    return false;
+  } else if (scl == 10) {
+    // SC_THIN_CIRRUS
+    return true;
+  } else if (scl == 11) {
+    // SC_SNOW_ICE
+    return false;
+  } else if (scl == 1) {
+    // SC_SATURATED_DEFECTIVE
+    return false;
+  } else if (scl == 2) {
+    // SC_DARK_FEATURE_SHADOW
+    return false;
+  }
+  return false;
+}
+
+"""
+
 # evalscript_lsm_only = """
 # function setup() {
 #   return {
