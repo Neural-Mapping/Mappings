3c84a42c1e85839043214f37a56359d004fa4046
Mafaz03 <mohdmafaz200303@gmail.com>
2025-03-14 19:06:57 +0530

    argparse

---
+++ d.py
@@ -0,0 +1,168 @@
+import argparse
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+from sentinelhub import SHConfig
+from sentinelhub import (
+    DataCollection,
+    MimeType,
+    SentinelHubRequest,
+    bbox_to_dimensions,
+)
+import folium
+from tqdm import tqdm
+import math
+
+from sentinelhub import SHConfig
+from dotenv import load_dotenv
+import os
+import numpy as np
+import datetime
+import os
+import matplotlib.pyplot as plt
+from sentinelhub import CRS, BBox, bbox_to_dimensions
+import cv2
+import ee
+
+config_sentinel = SHConfig(sh_client_id=os.environ.get("sh_client_id"), sh_client_secret=os.environ.get("sh_client_secret"))
+
+from mapping_automation import find_best_date, subtract_km_from_coordinates, get_cloud_coverage, search_available_dates, get_access_token, get_slope_elevation
+from scripts import *
+from utils import *
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def str_to_bool(value):
+    """Convert 'true'/'false' string to boolean."""
+    if value.lower() in ["true", "1", "yes"]:
+        return True
+    elif value.lower() in ["false", "0", "no"]:
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected (true/false).")
+
+true_color_path = "-True_Color-After.png"
+ndvi_before_path = "-NDVI-Before.png"
+ndvi_after_path = "-NDVI-After.png"
+slope_path = "-Slope.png"
+elevation_path = "-Elevation.png"
+NDWI_before_path = "-NDWI-Before.png"
+LSM_masked_path = "-LSM_True_Color-After.png"
+mask_path = "-dNDVI-masked.png"
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run grid scan with parameters.")
+
+    # Define required arguments
+    parser.add_argument("-pn", "--project_name", type=str, required=True, help="Name of the project (a new folder will be created)")
+    parser.add_argument("-d", "--date", type=str, required=True, help="Date around which the grid scan is to happen")
+    parser.add_argument("-lat", "--latitude", type=float, required=True, help="Latitude cordinate)")
+    parser.add_argument("-lon", "--longitude", type=float, required=True, help="Longitude cordinate")
+    parser.add_argument("-dim", "--dimention", type=int, required=True, help="Length of each side of each grid (in kilometer)")
+    parser.add_argument("-grids", "--num_grids", type=int, required=True, help="Number of grids (nxn)")
+    parser.add_argument("-res", "--resolution", type=int, required=True, help="Resolution of images being fetched")
+    parser.add_argument("-new_dates", "--get_new_dates_for_each_grid", type=str_to_bool, required=True, help="Use all resources (true/false)")
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    proj_name = args.project_name
+    lat = args.latitude
+    lon = args.longitude
+    box_dim = args.dimention
+    grid = args.num_grids
+    date = args.date
+    res = args.resolution
+
+    g = generate_grid(lat, lon, distance=box_dim*1000, grid_side=grid)
+
+    row = 0
+    col = 0
+    get_new_dates_for_each = True
+
+    _box_dim = 1000 if box_dim < 100 else box_dim
+    canvas_NDVI_Before = np.zeros(((grid) * _box_dim, (grid) * _box_dim, 3), dtype=np.uint8)
+    canvas_NDVI_After = np.zeros(((grid) * _box_dim, (grid) * _box_dim, 3), dtype=np.uint8)
+    canvas_True_Color_After = np.zeros(((grid) * _box_dim, (grid) * _box_dim, 3), dtype=np.uint8)
+    canvas_LSM_Only_After = np.zeros(((grid) * _box_dim, (grid) * _box_dim, 3), dtype=np.uint8)
+
+    for idx, i in enumerate(range(len(g))):
+
+        if get_new_dates_for_each or idx==0:
+            lat, lon = g[idx][0], g[idx][1]
+            print("Getting dates for new lat lon")
+            available_dates = search_available_dates(target_date=date,
+                                                    lat = lat,
+                                                    lon = lon)
+            available_dates_cloud_coverage = get_cloud_coverage(lat=lat, lon=lon, date_list=available_dates)
+            min_before_date, min_before_cc, min_after_date, min_after_cc = find_best_date({k: v for k, v in available_dates_cloud_coverage.items() if v is not None}, target_date=date)
+            print(min_before_date,":" , min_before_cc, min_after_date,":" ,min_after_cc)
+
+        y_start = _box_dim * col
+        y_end = _box_dim * (col + 1)
+        x_start = _box_dim * row
+        x_end = _box_dim * (row + 1)
+
+        print("NDVI_Before")
+        NDVI_Before = get_images(km=box_dim, grid=[g[i]], grid_dim=1, script=evalscript_NDVI, 
+            date_start=min_before_date, date_end=min_before_date, res=res, 
+            box_dim=box_dim)
+
+        canvas_NDVI_Before[y_start:y_end, x_start:x_end] = NDVI_Before
+
+        print("NDVI_After")
+        NDVI_After = get_images(km=box_dim, grid=[g[i]], grid_dim=1, script=evalscript_NDVI, 
+                    date_start=min_after_date, date_end=min_after_date, res=res, 
+                    box_dim=box_dim)
+        canvas_NDVI_After[y_start:y_end, x_start:x_end] = NDVI_After
+
+        print("True_Color_After")
+        True_Color_After = get_images(km=box_dim, grid=[g[i]], grid_dim=1, script=evalscript_True_Color, 
+                date_start=min_after_date, date_end=min_after_date, res=res, 
+                box_dim=box_dim)
+        canvas_True_Color_After[y_start:y_end, x_start:x_end] = True_Color_After
+
+        print("LSM_Only_After")
+        LSM_Only_After = get_images(km=box_dim, grid=[g[i]], grid_dim=1, script=evalscript_lsm_only, 
+                    date_start=min_after_date, date_end=min_after_date, res=res, 
+                    box_dim=box_dim)
+        canvas_LSM_Only_After[y_start:y_end, x_start:x_end] = LSM_Only_After
+
+        row += 1  # Move to the next column
+        if (idx + 1) % math.sqrt(len(g)) == 0:
+            print("----") 
+            col += 1  # Move to the next row
+            row = 0  # Reset column position
+
+    diff = canvas_NDVI_Before - canvas_NDVI_After
+    threshold = 0
+    tolerance = 60
+    mask = ((canvas_NDVI_Before > canvas_NDVI_After + tolerance) & (diff > threshold)).astype(np.uint8) * 255
+    mask = cv2.cvtColor(mask, cv2.COLOR_RGB2GRAY)
+    mask_resized = cv2.resize(mask, (mask.shape[0]//4, mask.shape[0]//4))
+    mask = cv2.resize(mask_resized, mask.shape)
+    mask = cv2.threshold(mask, 0, 255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+
+    no_mask = np.ones_like(canvas_True_Color_After) * 255
+    LMS_True_Color_dNDVI_not_Masked = canvas_True_Color_After.copy()
+    mask_condition = no_mask == 255
+    LMS_True_Color_dNDVI_not_Masked[mask_condition] = canvas_LSM_Only_After[mask_condition]
+    black_pixels = np.all(LMS_True_Color_dNDVI_not_Masked == [0, 0, 0], axis=-1)
+    LMS_True_Color_dNDVI_not_Masked[black_pixels] = canvas_True_Color_After[black_pixels]
+
+    LMS_True_Color_dNDVI_Masked = canvas_True_Color_After.copy()
+    LMS_True_Color_dNDVI_Masked[mask[:,:, None].repeat(3, -1) == 255] = canvas_LSM_Only_After[mask[:,:, None].repeat(3, -1) == 255]
+
+    black_mask = np.all(LMS_True_Color_dNDVI_Masked == [0, 0, 0], axis=-1)
+    output = LMS_True_Color_dNDVI_Masked.copy()
+    LMS_True_Color_dNDVI_Masked[black_mask] = canvas_True_Color_After[black_mask]
+
+    os.makedirs(proj_name, exist_ok=True)
+    plt.imsave(f"{proj_name}/{proj_name}{true_color_path}", canvas_True_Color_After)
+    plt.imsave(f"{proj_name}/{proj_name}{ndvi_before_path}", canvas_NDVI_Before)
+    plt.imsave(f"{proj_name}/{proj_name}{ndvi_after_path}", canvas_NDVI_After)
+    plt.imsave(f"{proj_name}/{proj_name}-dNDVI_Mask.png", mask, cmap="gray")
+    plt.imsave(f"{proj_name}/{proj_name}{LSM_masked_path}", LMS_True_Color_dNDVI_Masked)
+    plt.imsave(f"{proj_name}/{proj_name}LSM_Only.png", canvas_LSM_Only_After)
--- mapping_automation.py
+++ mapping_automation.py
@@ -1,22 +1,3 @@
-print(r"""
-         ,-.
-        / \  `.  __..-,O
-       :   \ --''_..-'.'
-       |    . .-' `. '.
-       :     .     .`.'
-        \     `.  /  ..
-         \      `.   ' .
-          `,       `.   \
-         ,|,`.        `-.\
-        '.||  ``-...__..-`
-         |  |
-         |__|
-         /||\
-        //||\\
-       // || \\
-    __//__||__\\__
-   '--------------' 
-""")
 import requests
 from datetime import datetime, timedelta
 import os
@@ -196,172 +177,193 @@ def get_slope_elevation(g):
 
 #### START #### 
 
-grid = 1
-box_dim = 5 # km
+if __name__ == "__main__":
+    print(r"""
+            ,-.
+            / \  `.  __..-,O
+        :   \ --''_..-'.'
+        |    . .-' `. '.
+        :     .     .`.'
+            \     `.  /  ..
+            \      `.   ' .
+            `,       `.   \
+            ,|,`.        `-.\
+            '.||  ``-...__..-`
+            |  |
+            |__|
+            /||\
+            //||\\
+        // || \\
+        __//__||__\\__
+    '--------------' 
+    """)
 
-columns_to_keep = ["event_date", "event_title", "latitude", "longitude"]
-nasa_landslides = pd.read_csv("nasa_global_landslide_catalog_point.csv", parse_dates=["event_date"], usecols=columns_to_keep)
-nasa_landslides["event_date"] = pd.to_datetime(nasa_landslides["event_date"], errors="coerce")
-df = nasa_landslides.dropna(subset=["event_date"])
-ten_days_ago = datetime.today() - timedelta(days=10)
-nasa_landslides = nasa_landslides[(nasa_landslides["event_date"] > "2018-01-01") & (nasa_landslides["event_date"] < ten_days_ago.date().strftime("%Y-%m-%d"))]
-nasa_landslides["event_date"] = nasa_landslides["event_date"].dt.date
+    grid = 1
+    box_dim = 5 # km
 
-for landlside_row in range(nasa_landslides.shape[0]):
-    project_name = nasa_landslides.iloc[landlside_row].event_title
-    project_name = re.sub(r'[\/:*?"<>|]', '_', str(project_name)).strip()
-    print("*"*20)
+    columns_to_keep = ["event_date", "event_title", "latitude", "longitude"]
+    nasa_landslides = pd.read_csv("nasa_global_landslide_catalog_point.csv", parse_dates=["event_date"], usecols=columns_to_keep)
+    nasa_landslides["event_date"] = pd.to_datetime(nasa_landslides["event_date"], errors="coerce")
+    df = nasa_landslides.dropna(subset=["event_date"])
+    ten_days_ago = datetime.today() - timedelta(days=10)
+    nasa_landslides = nasa_landslides[(nasa_landslides["event_date"] > "2018-01-01") & (nasa_landslides["event_date"] < ten_days_ago.date().strftime("%Y-%m-%d"))]
+    nasa_landslides["event_date"] = nasa_landslides["event_date"].dt.date
 
-    with open("do_not_attend.txt", "r") as log_file:
-        content = [line.strip() for line in log_file]
-        if project_name in content: 
-            print(f"{project_name} in DO NOT ATTEND: reason: Unknown") # havent figured this out yet lol
+    for landlside_row in range(nasa_landslides.shape[0]):
+        project_name = nasa_landslides.iloc[landlside_row].event_title
+        project_name = re.sub(r'[\/:*?"<>|]', '_', str(project_name)).strip()
+        print("*"*20)
+
+        with open("do_not_attend.txt", "r") as log_file:
+            content = [line.strip() for line in log_file]
+            if project_name in content: 
+                print(f"{project_name} in DO NOT ATTEND: reason: Unknown") # havent figured this out yet lol
+                continue
+
+        # with open("Mapping Automation Completions.txt", "r") as log_file:
+        #     content = [line.strip() for line in log_file]
+        #     if project_name in content: 
+        #         print(f"Skipping: {project_name}: Already exists")
+        #         continue
+        
+        print("GETTING BEST DATES\n")
+        date = nasa_landslides.iloc[landlside_row].event_date.strftime("%Y-%m-%d")
+        lat, lon = subtract_km_from_coordinates(nasa_landslides.iloc[1].latitude, nasa_landslides.iloc[landlside_row].longitude, 1.5)
+        available_dates = search_available_dates(target_date=nasa_landslides.iloc[landlside_row].event_date.strftime("%Y-%m-%d"),
+                                                lat = lat,
+                                                lon = lon)
+        if available_dates == None: 
+            print(f"Skipping: {project_name}: Dates not available")
+            with open("do_not_attend.txt", "a") as log_file:
+                log_file.write(f"{project_name}\n")
             continue
+        available_dates_cloud_coverage = get_cloud_coverage(lat=lat, lon=lon, date_list=available_dates)
+        
+        min_before_date, min_before_cc, min_after_date, min_after_cc = find_best_date(available_dates_cloud_coverage, target_date=date)
 
-    # with open("Mapping Automation Completions.txt", "r") as log_file:
-    #     content = [line.strip() for line in log_file]
-    #     if project_name in content: 
-    #         print(f"Skipping: {project_name}: Already exists")
-    #         continue
-    
-    print("GETTING BEST DATES\n")
-    date = nasa_landslides.iloc[landlside_row].event_date.strftime("%Y-%m-%d")
-    lat, lon = subtract_km_from_coordinates(nasa_landslides.iloc[1].latitude, nasa_landslides.iloc[landlside_row].longitude, 1.5)
-    available_dates = search_available_dates(target_date=nasa_landslides.iloc[landlside_row].event_date.strftime("%Y-%m-%d"),
-                                            lat = lat,
-                                            lon = lon)
-    if available_dates == None: 
-        print(f"Skipping: {project_name}: Dates not available")
+        min_lat, min_lon  = lat, lon
+        start_date = min_before_date
+        end_date = min_after_date
+        # project_name = project_name
+        if start_date == "" or end_date == "":
+            print(f"Skipping: {project_name}: start date or end date not found")
+            # shutil.rmtree(project_name)
+            with open("do_not_attend.txt", "a") as log_file:
+                log_file.write(f"{project_name}\n")
+            continue
+        
+        os.makedirs(project_name, exist_ok=True)
+        with open(f'{project_name}/log.txt', 'w') as f:
+            f.write(f'Project Name: {project_name}\n')
+            f.write(f'Grid size: {grid} x {grid}\n')
+            f.write(f'Box Dimension: {box_dim} km\n')
+            f.write(f'Bounding Box: {min_lat}, {min_lon}\n')
+            f.write(f'Start Date: {start_date}\n')
+            f.write(f'End Date: {end_date}\n')
+        
+        g = generate_grid(min_lat, min_lon, distance=box_dim*1000, grid_side=grid)
+        
+        print("GETTING DIFFERENT FEATURES\n")
+        NDVI_Before = get_images(km=box_dim, grid=g, grid_dim=grid, script=evalscript_NDVI, 
+            date_start=start_date, date_end=start_date, res=1000, 
+            box_dim=box_dim, file_name=f"{project_name}/{project_name}-NDVI-Before")
+
+        NDVI_After = get_images(km=box_dim, grid=g, grid_dim=grid, script=evalscript_NDVI, 
+                date_start=end_date, date_end=end_date, res=1000, 
+                box_dim=box_dim, file_name=f"{project_name}/{project_name}-NDVI-After")
+
+        True_Color_After = get_images(km=box_dim, grid=g, grid_dim=grid, script=evalscript_True_Color, 
+                date_start=end_date, date_end=end_date, res=1000, 
+                box_dim=box_dim, file_name=f"{project_name}/{project_name}-True_Color-After")
+
+        LSM_Only_After = get_images(km=box_dim, grid=g, grid_dim=grid, script=evalscript_lsm_only, 
+                date_start=end_date, date_end=end_date, res=1000, 
+                box_dim=box_dim, file_name=f"{project_name}/{project_name}-LSM_Only-After")
+
+        NDWI_Before  = get_images(km=box_dim, grid=g, grid_dim=grid, script=evalscript_NDWI, 
+                date_start=end_date, date_end=end_date, res=1000, 
+                box_dim=box_dim, file_name=f"{project_name}/{project_name}-NDWI-Before")
+
+        slope_arr, elevation_arr = get_slope_elevation(g)
+        slope_arr = cv2.resize(slope_arr, (1000,1000))
+        elevation_arr = cv2.resize(elevation_arr, (1000,1000))
+
+        plt.imsave(f"{project_name}/{project_name}-Slope.png", slope_arr)
+        plt.imsave(f"{project_name}/{project_name}-Elevation.png", elevation_arr)
+
+        diff = NDVI_Before - NDVI_After
+        threshold = 0
+        tolerance = 60
+        mask = ((NDVI_Before > NDVI_After + tolerance) & (diff > threshold)).astype(np.uint8) * 255
+        mask = cv2.cvtColor(mask, cv2.COLOR_RGB2GRAY)
+        mask_resized = cv2.resize(mask, (mask.shape[0]//4, mask.shape[0]//4))
+        mask = cv2.resize(mask_resized, mask.shape)
+        mask = cv2.threshold(mask, 0, 255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+
+        plt.imsave(f"{project_name}/{project_name}-dNDVI-masked.png", mask, cmap='gray')
+
+        no_mask = np.ones_like(True_Color_After) * 255
+        LMS_True_Color_dNDVI_not_Masked = True_Color_After.copy()
+        mask_condition = no_mask == 255
+        LMS_True_Color_dNDVI_not_Masked[mask_condition] = LSM_Only_After[mask_condition]
+        black_pixels = np.all(LMS_True_Color_dNDVI_not_Masked == [0, 0, 0], axis=-1)
+        LMS_True_Color_dNDVI_not_Masked[black_pixels] = True_Color_After[black_pixels]
+
+        # plt.imshow(LMS_True_Color_dNDVI_not_Masked); plt.axis("off")
+        plt.title("LMS True Color Not Masked")
+        plt.imsave(f"{project_name}/{project_name}-LMS_True_Color_dNDVI_Not_Masked.png", LMS_True_Color_dNDVI_not_Masked)
+
+        LMS_True_Color_dNDVI_Masked = True_Color_After.copy()
+        LMS_True_Color_dNDVI_Masked[mask[:,:, None].repeat(3, -1) == 255] = LSM_Only_After[mask[:,:, None].repeat(3, -1) == 255]
+
+        # replace black spots with true color (i should have figured this out since the start lol)
+        black_mask = np.all(LMS_True_Color_dNDVI_Masked == [0, 0, 0], axis=-1)
+        output = LMS_True_Color_dNDVI_Masked.copy()
+        LMS_True_Color_dNDVI_Masked[black_mask] = True_Color_After[black_mask]
+
+        # plt.imshow(LMS_True_Color_dNDVI_Masked); plt.axis("off")
+        plt.title("LMS True Color Masked")
+        plt.imsave(f"{project_name}/{project_name}-LMS_True_Color_dNDVI_Masked.png", LMS_True_Color_dNDVI_Masked)
+
+        fig, axes = plt.subplots(2, 3, figsize=(20, 10))
+
+        # First row
+        axes[0, 0].imshow(NDVI_Before)
+        axes[0, 0].set_title("NDVI Before")
+        axes[0, 0].axis("off")
+
+        axes[0, 1].imshow(NDVI_After)
+        axes[0, 1].set_title("NDVI After")
+        axes[0, 1].axis("off")
+
+        axes[0, 2].imshow(True_Color_After)
+        axes[0, 2].set_title("True Color After")
+        axes[0, 2].axis("off")
+
+        # Second row
+        axes[1, 0].imshow(LSM_Only_After)
+        axes[1, 0].set_title("LSM Only After")
+        axes[1, 0].axis("off")
+
+        # Add the mask visualization in grayscale
+        axes[1, 1].imshow(mask, cmap="gray")
+        axes[1, 1].set_title("Mask")
+        axes[1, 1].axis("off")
+
+        # Compute masked image
+        axes[1, 2].imshow(LMS_True_Color_dNDVI_Masked, cmap="gray")
+        axes[1, 2].set_title("LMS True Color Masked")
+        axes[1, 2].axis("off")
+
+        plt.tight_layout()
+        plt.savefig(f"{project_name}/{project_name}-combined_image.png")
+        # plt.show()
+
+        with open("Mapping Automation Completions.txt", "a") as log_file:
+            log_file.write(f"{project_name}\n")
         with open("do_not_attend.txt", "a") as log_file:
             log_file.write(f"{project_name}\n")
-        continue
-    available_dates_cloud_coverage = get_cloud_coverage(lat=lat, lon=lon, date_list=available_dates)
-    
-    min_before_date, min_before_cc, min_after_date, min_after_cc = find_best_date(available_dates_cloud_coverage, target_date=date)
+                        
+        plt.close()
 
-    min_lat, min_lon  = lat, lon
-    start_date = min_before_date
-    end_date = min_after_date
-    # project_name = project_name
-    if start_date == "" or end_date == "":
-        print(f"Skipping: {project_name}: start date or end date not found")
-        # shutil.rmtree(project_name)
-        with open("do_not_attend.txt", "a") as log_file:
-            log_file.write(f"{project_name}\n")
-        continue
-    
-    os.makedirs(project_name, exist_ok=True)
-    with open(f'{project_name}/log.txt', 'w') as f:
-        f.write(f'Project Name: {project_name}\n')
-        f.write(f'Grid size: {grid} x {grid}\n')
-        f.write(f'Box Dimension: {box_dim} km\n')
-        f.write(f'Bounding Box: {min_lat}, {min_lon}\n')
-        f.write(f'Start Date: {start_date}\n')
-        f.write(f'End Date: {end_date}\n')
-    
-    g = generate_grid(min_lat, min_lon, distance=box_dim*1000, grid_side=grid)
-    
-    print("GETTING DIFFERENT FEATURES\n")
-    NDVI_Before = get_images(km=box_dim, grid=g, grid_dim=grid, script=evalscript_NDVI, 
-           date_start=start_date, date_end=start_date, res=1000, 
-           box_dim=box_dim, file_name=f"{project_name}/{project_name}-NDVI-Before")
-
-    NDVI_After = get_images(km=box_dim, grid=g, grid_dim=grid, script=evalscript_NDVI, 
-            date_start=end_date, date_end=end_date, res=1000, 
-            box_dim=box_dim, file_name=f"{project_name}/{project_name}-NDVI-After")
-
-    True_Color_After = get_images(km=box_dim, grid=g, grid_dim=grid, script=evalscript_True_Color, 
-            date_start=end_date, date_end=end_date, res=1000, 
-            box_dim=box_dim, file_name=f"{project_name}/{project_name}-True_Color-After")
-
-    LSM_Only_After = get_images(km=box_dim, grid=g, grid_dim=grid, script=evalscript_lsm_only, 
-            date_start=end_date, date_end=end_date, res=1000, 
-            box_dim=box_dim, file_name=f"{project_name}/{project_name}-LSM_Only-After")
-
-    NDWI_Before  = get_images(km=box_dim, grid=g, grid_dim=grid, script=evalscript_NDWI, 
-            date_start=end_date, date_end=end_date, res=1000, 
-            box_dim=box_dim, file_name=f"{project_name}/{project_name}-NDWI-Before")
-
-    slope_arr, elevation_arr = get_slope_elevation(g)
-    slope_arr = cv2.resize(slope_arr, (1000,1000))
-    elevation_arr = cv2.resize(elevation_arr, (1000,1000))
-
-    plt.imsave(f"{project_name}/{project_name}-Slope.png", slope_arr)
-    plt.imsave(f"{project_name}/{project_name}-Elevation.png", elevation_arr)
-
-    diff = NDVI_Before - NDVI_After
-    threshold = 0
-    tolerance = 60
-    mask = ((NDVI_Before > NDVI_After + tolerance) & (diff > threshold)).astype(np.uint8) * 255
-    mask = cv2.cvtColor(mask, cv2.COLOR_RGB2GRAY)
-    mask_resized = cv2.resize(mask, (mask.shape[0]//4, mask.shape[0]//4))
-    mask = cv2.resize(mask_resized, mask.shape)
-    mask = cv2.threshold(mask, 0, 255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
-
-    plt.imsave(f"{project_name}/{project_name}-dNDVI-masked.png", mask, cmap='gray')
-
-    no_mask = np.ones_like(True_Color_After) * 255
-    LMS_True_Color_dNDVI_not_Masked = True_Color_After.copy()
-    mask_condition = no_mask == 255
-    LMS_True_Color_dNDVI_not_Masked[mask_condition] = LSM_Only_After[mask_condition]
-    black_pixels = np.all(LMS_True_Color_dNDVI_not_Masked == [0, 0, 0], axis=-1)
-    LMS_True_Color_dNDVI_not_Masked[black_pixels] = True_Color_After[black_pixels]
-
-    # plt.imshow(LMS_True_Color_dNDVI_not_Masked); plt.axis("off")
-    plt.title("LMS True Color Not Masked")
-    plt.imsave(f"{project_name}/{project_name}-LMS_True_Color_dNDVI_Not_Masked.png", LMS_True_Color_dNDVI_not_Masked)
-
-    LMS_True_Color_dNDVI_Masked = True_Color_After.copy()
-    LMS_True_Color_dNDVI_Masked[mask[:,:, None].repeat(3, -1) == 255] = LSM_Only_After[mask[:,:, None].repeat(3, -1) == 255]
-
-    # replace black spots with true color (i should have figured this out since the start lol)
-    black_mask = np.all(LMS_True_Color_dNDVI_Masked == [0, 0, 0], axis=-1)
-    output = LMS_True_Color_dNDVI_Masked.copy()
-    LMS_True_Color_dNDVI_Masked[black_mask] = True_Color_After[black_mask]
-
-    # plt.imshow(LMS_True_Color_dNDVI_Masked); plt.axis("off")
-    plt.title("LMS True Color Masked")
-    plt.imsave(f"{project_name}/{project_name}-LMS_True_Color_dNDVI_Masked.png", LMS_True_Color_dNDVI_Masked)
-
-    fig, axes = plt.subplots(2, 3, figsize=(20, 10))
-
-    # First row
-    axes[0, 0].imshow(NDVI_Before)
-    axes[0, 0].set_title("NDVI Before")
-    axes[0, 0].axis("off")
-
-    axes[0, 1].imshow(NDVI_After)
-    axes[0, 1].set_title("NDVI After")
-    axes[0, 1].axis("off")
-
-    axes[0, 2].imshow(True_Color_After)
-    axes[0, 2].set_title("True Color After")
-    axes[0, 2].axis("off")
-
-    # Second row
-    axes[1, 0].imshow(LSM_Only_After)
-    axes[1, 0].set_title("LSM Only After")
-    axes[1, 0].axis("off")
-
-    # Add the mask visualization in grayscale
-    axes[1, 1].imshow(mask, cmap="gray")
-    axes[1, 1].set_title("Mask")
-    axes[1, 1].axis("off")
-
-    # Compute masked image
-    axes[1, 2].imshow(LMS_True_Color_dNDVI_Masked, cmap="gray")
-    axes[1, 2].set_title("LMS True Color Masked")
-    axes[1, 2].axis("off")
-
-    plt.tight_layout()
-    plt.savefig(f"{project_name}/{project_name}-combined_image.png")
-    # plt.show()
-
-    with open("Mapping Automation Completions.txt", "a") as log_file:
-        log_file.write(f"{project_name}\n")
-    with open("do_not_attend.txt", "a") as log_file:
-        log_file.write(f"{project_name}\n")
-                       
-    plt.close()
-
-    
+        
